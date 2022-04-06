5fc780bf687118a0a0a9357f5da5988101dce832
padoca <padominca@gmail.com>
2022-04-06 11:03:31 +0200

    Order by

--- src/main/java/es/inditex/inditex/repository/PricesRepository.java
+++ src/main/java/es/inditex/inditex/repository/PricesRepository.java
@@ -13,7 +13,8 @@ public interface PricesRepository extends CrudRepository<Prices, Integer> {
 
   @Query(value = "SELECT p FROM Prices p WHERE (:product_id is null OR p.productId = :product_id) "
       + "AND (:brand_id is null OR p.brandId = :brand_id) "
-      + "AND (:application_date is null OR (p.startDate <= :application_date AND p.endDate > :application_date) )")
+      + "AND (:application_date is null OR (p.startDate <= :application_date AND p.endDate > :application_date) )"
+      + "ORDER BY p.startDate desc")
   List<Prices> findByFilter(@Param("application_date") final Timestamp applicationDate, @Param("product_id") final Integer productId,
       @Param("brand_id") final Integer brandId);
 }
